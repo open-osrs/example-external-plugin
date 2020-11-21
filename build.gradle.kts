303020e79aa8386112350f22509081465c161d1e
tha23rd <>
2020-11-21 12:16:54 -0500

    building

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
     checkstyle
 }
 
-project.extra["GithubUrl"] = "http://github.com/<INSERT NAME>/<INSERT REPOSITORY>"
+project.extra["GithubUrl"] = "http://github.com/tha23rd/example-external-plugin"
 
 apply<BootstrapPlugin>()
 
 subprojects {
     group = "com.example"
 
-    project.extra["PluginProvider"] = ""
+    project.extra["PluginProvider"] = "tha23rd"
     project.extra["ProjectSupportUrl"] = ""
     project.extra["PluginLicense"] = "3-Clause BSD License"
 
